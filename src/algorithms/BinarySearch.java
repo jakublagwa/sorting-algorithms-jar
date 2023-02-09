d2325e75eb556ce221848b28abe7e9bed93f0920
Jakub Łągwa <kklagwa@gmail.com>
2023-02-10 00:54:32 +0100

    making progress

--- src/algorithms/BinarySearch.java
+++ src/algorithms/BinarySearch.java
@@ -1,5 +1,12 @@
 package algorithms;
 
 public class BinarySearch {
-	
+	public static int[] binarySearch(int array[], int target) {
+		int left=0;
+		int right=array.length;
+		while(left<=right) {
+			
+		}
+		return array;
+	}
 }
